a0a1bba911ee9879dc6bf61802dcc3edd138f1db
Warano02 <carineteoi@gmail.com>
2025-06-09 12:19:44 +0100

    k

--- src/components/samsungs/SamHome.jsx
+++ src/components/samsungs/SamHome.jsx
@@ -164,6 +164,9 @@ function SamHome() {
             )}. From ${product.price} before trade in.`,
           }))}
       />
+      <Marque cardData={products.filter(
+        (elem) => elem.category === "phone"
+      )} />
     </>
   );
 }
@@ -282,5 +285,46 @@ export function ProductScroll({ products, label, scrollable }) {
     </div>
   );
 }
+
+const Marque = ({ cardData }) => {
+  const [stopScroll, setStopScroll] = useState(false);
+
+  return (
+    <>
+      <style>{`
+                .marquee-inner {
+                    animation: marqueeScroll linear infinite;
+                }
+
+                @keyframes marqueeScroll {
+                    0% {
+                        transform: translateX(0%);
+                    }
+
+                    100% {
+                        transform: translateX(-50%);
+                    }
+                }
+            `}</style>
+
+      <div className="overflow-hidden w-full relative  mx-auto" onMouseEnter={() => setStopScroll(true)} onMouseLeave={() => setStopScroll(false)}>
+        <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-[#F5F7FF] to-transparent" />
+        <div className="marquee-inner flex w-fit" style={{ animationPlayState: stopScroll ? "paused" : "running", animationDuration: cardData.length * 2500 + "ms" }}>
+          <div className="flex">
+            {[...cardData, ...cardData].map((card, index) => (
+              <Link to={`/collection/samsung/${card.category}/${card.id}`} key={index} className="w-80 mx-4 h-[30rem] relative group hover:scale-90 transition-all duration-300">
+                <img src={card.logo} alt="card" className="w-full h-full object-cover" />
+                <div className="flex items-center justify-center px-4 opacity-0 group-hover:opacity-100 transition-all duration-300 absolute bottom-0 backdrop-blur-md left-0 w-full h-full bg-black/20">
+                  <p className="text-white text-xl font-semibold text-center">{card.desc || card.name}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-[#F5F7FF] to-transparent" />
+      </div>
+    </>
+  );
+};
 
 export default SamHome;
